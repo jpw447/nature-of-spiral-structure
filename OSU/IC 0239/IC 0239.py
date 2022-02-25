b2e2f983386d129871c3c78297dc8702fc4356f2
jpw447 <joepw1@hotmail.com>
2022-02-25 00:32:14 +0000

    sliders attempt

--- OSU/IC 0239/IC 0239.py
+++ OSU/IC 0239/IC 0239.py
@@ -8,27 +8,22 @@ from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import widgets
 plt.style.use(astropy_mpl_style)
 
-def max_brightness_change(val):
-    plt.clear()
+galaxy_name = 'ic0239'
 
-full_image = fits.getdata("OSU\data\survey\ByFilter\B_band\ic4444b.fits")
+full_image = fits.getdata("OSU\data\survey\ByFilter\B_band\{}b.fits".format(galaxy_name))
 log_image= np.log10(full_image)
 
 histogram_log = plt.hist(log_image.ravel(), bins='auto')
-plt.close()
 largest_intensity = np.max(histogram_log[0])
 max_brightness = histogram_log[1][np.where(histogram_log[0] == largest_intensity)]
 
-
-fig = plt.figure()
-ax = fig.gca()
-full_im = ax.imshow(log_image, cmap='gray', vmin=max_brightness, vmax=2.5)
-max_sax = plt.axes([0.1, 0.1, 0.8, 0.05]) 
-max_slider = widgets.Slider(max_sax, 'Max Brightness', 0, 3*max_brightness)
-max_slider.on_changed(max_brightness_change)
-ax.set_title("IC 4444 (rescaled)")
-plt.grid(False)
+fig, ax = plt.subplots(1,2)
+pos_im = ax[0].imshow(log_image, cmap='gray', vmin=2, vmax=5)
+full_im = ax[1].imshow(log_image, cmap='gray', vmin=max_brightness, vmax=1.7)
+ax[0].set_title(galaxy_name)
+ax[0].grid(False)
+ax[1].grid(False)
+ax[1].set_title(str(galaxy_name)+" (rescaled)")
 plt.show()
--- OSU/IC 4444/IC 4444.py
+++ OSU/IC 4444/IC 4444.py
@@ -8,27 +8,20 @@ from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import widgets
 plt.style.use(astropy_mpl_style)
-
-def max_brightness_change(val):
-    plt.clear()
 
 full_image = fits.getdata("OSU\data\survey\ByFilter\B_band\ic4444b.fits")
 log_image= np.log10(full_image)
 
 histogram_log = plt.hist(log_image.ravel(), bins='auto')
-plt.close()
 largest_intensity = np.max(histogram_log[0])
 max_brightness = histogram_log[1][np.where(histogram_log[0] == largest_intensity)]
 
-
-fig = plt.figure()
-ax = fig.gca()
-full_im = ax.imshow(log_image, cmap='gray', vmin=max_brightness, vmax=2.5)
-max_sax = plt.axes([0.1, 0.1, 0.8, 0.05]) 
-max_slider = widgets.Slider(max_sax, 'Max Brightness', 0, 3*max_brightness)
-max_slider.on_changed(max_brightness_change)
-ax.set_title("IC 4444 (rescaled)")
-plt.grid(False)
+fig, ax = plt.subplots(1,2)
+pos_im = ax[0].imshow(full_image, cmap='gray', vmin=10, vmax=50)
+full_im = ax[1].imshow(log_image, cmap='gray', vmin=max_brightness, vmax=2.5)
+ax[0].set_title("IC 4444")
+ax[0].grid(False)
+ax[1].grid(False)
+ax[1].set_title("IC 4444 (rescaled)")
 plt.show()
