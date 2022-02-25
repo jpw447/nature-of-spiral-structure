8af643638320e11136cca64ac82af7f8ab8b42bf
jpw447 <joepw1@hotmail.com>
2022-02-25 13:55:55 +0000

    Update OSU Analysis.py

--- OSU/OSU Analysis.py
+++ OSU/OSU Analysis.py
@@ -8,27 +8,31 @@ from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import widgets
+from matplotlib.widgets import Slider
 plt.style.use(astropy_mpl_style)
 
-def max_brightness_change(val):
-    plt.clear()
+def update_min(val):
+    full_im = ax.imshow(log_image, cmap='gray', vmin=val, vmax=2.5)
+    ax.canvas.draw_idle()
 
 full_image = fits.getdata("OSU\data\survey\ByFilter\B_band\ic4444b.fits")
 log_image= np.log10(full_image)
 
-histogram_log = plt.hist(log_image.ravel(), bins='auto')
+count, intensity, _ = plt.hist(log_image.ravel(), bins='auto')
 plt.close()
-largest_intensity = np.max(histogram_log[0])
-max_brightness = histogram_log[1][np.where(histogram_log[0] == largest_intensity)]
-
+largest_intensity = np.max(count)
+min_brightness = intensity[np.where(count == largest_intensity)][0]
+max_brightness = 2.5
 
 fig = plt.figure()
 ax = fig.gca()
-full_im = ax.imshow(log_image, cmap='gray', vmin=max_brightness, vmax=2.5)
-max_sax = plt.axes([0.1, 0.1, 0.8, 0.05]) 
-max_slider = widgets.Slider(max_sax, 'Max Brightness', 0, 3*max_brightness)
-max_slider.on_changed(max_brightness_change)
+full_im = ax.imshow(log_image, cmap='gray', vmin=min_brightness, vmax=max_brightness)
+ax_min_slider = plt.axes([0.20, 0.01, 0.65, 0.03])
+min_slider = Slider(ax_min_slider, 'min', 1, 3*min_brightness, valinit=min_brightness)
+
+
+min_slider.on_changed(update_min)
+
 ax.set_title("IC 4444 (rescaled)")
 plt.grid(False)
 plt.show()
