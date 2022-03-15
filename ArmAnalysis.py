c8d89efa8c2c777ceece608a954135226729d6c4
jpw447 <joepw1@hotmail.com>
2022-03-15 14:43:19 +0000

    Modularising

--- ArmAnalysis.py
+++ ArmAnalysis.py
@@ -2,14 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-def image_display(axis, name, file_name, colour_min=1.7, colour_max=2.7, file_path=''):
+plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False)
+
+def histogram_limiter(percentage, count, intensity):
+    '''
+    Finds the maximum brightness in the histogram, max_brightness.
+    intensity = x-values
+    count = y-values
+    Works by finding the histogram indices where the brightness exceeds the 
+    specified threshold. The edge idices are then returned to give an
+    appropriate brightness range for image display.
+    '''
+    threshold = percentage*np.max(count)
+    bright_indices = np.where(count > threshold)[0]
+    lower_bound, upper_bound = bright_indices[0], bright_indices[-1]
+    vmin, vmax = intensity[lower_bound], intensity[upper_bound]
+    
+    return vmin, vmax
+
+def image_display(path, galaxy_name, colour_band):
     '''
     Loads a FITS file image and rescales it for visualisation using base 10 logarithm.
     Plots using plt.imshow from matplotlib
     '''
-    full_image = fits.getdata(file_path+file_name)
+    '''
+    This first name-grabbing section will later need updating to filtering through
+    a list of filenames provided to it for each colour band
+    '''
+    name = input("Type the galaxy name in the correct file format: ")
+    
+    try:
+        full_image = fits.getdata(str(path)+"\\{}.fits".format(name))
+    except FileNotFoundError:
+        print("ERROR: File not found. Check the path provided and ensure it doesn't end in '\\\\', and\
+              that you've provided the correct file name.")
+        return
+    
     log_image = np.log10(full_image)
-    axis.imshow(log_image, cmap='gray', vmin=1.5, vmax=2.7)
-    axis.set_title("OSU Image of "+name)
+    
+    # Creates an histogram plot and finds the brightness where most pixels lie (histogram peak)
+    fig_hist = plt.figure()
+    ax_hist = fig_hist.gca()
+    pixel_count, pixel_intensity, _ = plt.hist(log_image.flatten(), bins='auto')
+    ax_hist.set_title("Image Histogram")
+    
+    percentage = 0.005
+    vmin, vmax = histogram_limiter(percentage, pixel_count, pixel_intensity)
+    
+    # The image with a calculated, restricted brightness range
+    fig_image = plt.figure()
+    ax_image = fig_image.gca()
+    ax_image.imshow(log_image, cmap='gray', vmin=vmin, vmax=vmax)
+    ax_image.set_title("{} in {}-Band".format(galaxy_name, colour_band))
+    plt.grid(False)
     plt.show()
-    return full_image
+
--- OSU Analysis.py
+++ OSU Analysis.py
@@ -1,39 +1,11 @@
-from astropy.io import fits
-from astropy.visualization import astropy_mpl_style
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-plt.style.use(astropy_mpl_style)
+import ArmAnalysis as aa
 
-# Loads the image and rescales the data using base 10 logarithms
-name = input("Type the galaxy name in the correct file format: ")
-full_image = fits.getdata("OSU\data\survey\ByFilter\B_band\\{}.fits".format(name))
-log_image = np.log10(full_image)
-
-# Creates an histogram plot and finds the brightness where most pixels lie (histogram peak)
-fig_hist = plt.figure()
-ax_hist = fig_hist.gca()
-count, intensity, _ = plt.hist(log_image.flatten(), bins='auto')
-ax_hist.set_title("Image Histogram")
-
-'''
-This finds the maximum brightness in the histogram, max_brightness.
-max_brightness is the brightness with the most pixels
-'''
-# Finds the maximum brightness in the histogram, max_brightness.
-# intensity = x-values
-# count = y-values
-percentage = 0.005
-max_brightness = intensity[np.argmax(count)]                        # Brightness with most pixels
-threshold = percentage*np.max(count)                                # Arbitrary threshold
-bright_indices = np.where(count > threshold)[0]                     # Histogram indices where brightness exceeds threshold
-lower_bound, upper_bound = bright_indices[0], bright_indices[-1]    # Min/max of above
-vmin, vmax = intensity[lower_bound], intensity[upper_bound]         # Brightness range
-
-# The bright image above the specified threshold
-fig_image = plt.figure()
-ax_image = fig_image.gca()
-image = ax_image.imshow(log_image, cmap='gray', vmin=vmin, vmax=vmax)
-ax_image.set_title("NGC 4995 (above "+str(threshold*100)+"% intensity)")
-plt.grid(False)
-plt.show()
+if __name__ == "__main__":
+    band = input("Which colour band do you want to look at?")
+    filename = input("Which filename would you like to start with? (Do not include file extension)")
+    
+    # Going to need a .txt list of all the galaxy names too
+    galaxy_name = "NGC 5054"
+    
+    path = "C:\\Users\\joepw\\Documents\\Year 3\\Nature of Spiral Structure\\OSU\\data\\survey\ByFilter\\{}_band".format(band)
+    aa.image_display(path, galaxy_name, band)
